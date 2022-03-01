05656f21c1a8759758c795e2eef2673908b180aa
Ag0r9 <51130332+Ag0r9@users.noreply.github.com>
2022-03-01 20:45:00 +0100

    uploading pictures added

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import streamlit
+import streamlit as st
+from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
 
-"""
-# Hello World!
-"""
+st.title('Upload a photo')
+
+uploaded_file = st.file_uploader('Choose an image...', type=['png', 'jpg', 'jpeg'])
+if uploaded_file is not None:
+    st.write('')
+    st.write('Detecting...')
+    # img = Image.open('data/jpgs/Kropka.jpg')
+    img = Image.open(uploaded_file).convert('L')
+    st.image(img)
